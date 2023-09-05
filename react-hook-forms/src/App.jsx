677f04e751b01e0ac34862ddb3ef2cf672dbe4f2
htetsoemoe <soemoehtet.mdy@gmail.com>
2023-09-05 16:47:31 +0630

    project setup with tailwind css

---
+++ react-hook-forms/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import BasicReactHookForm from './FormComponents/BasicReactHookForm'
+
+const App = () => {
+  return (
+    <div>
+      <BasicReactHookForm />
+    </div>
+  )
+}
+
+export default App
---
+++ react-hook-forms/src/FormComponents/BasicReactHookForm.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const BasicReactHookForm = () => {
+  return (
+    <div className='w-96 mx-auto'>
+          <h1 className='text-2xl text-blue-900'>Hello React Hook Form</h1>
+    </div>
+  )
+}
+
+export default BasicReactHookForm
